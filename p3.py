41d8953a80f43fbca2d679b5d144c93cd4f33d02
tinnakorn941 <34223443+tinnakorn941@users.noreply.github.com>
2019-02-10 11:16:15 +0700

    Update p3.py

--- p3.py
+++ p3.py
@@ -14,7 +14,7 @@ botStart = time.time()
 #==============================================================================#
 line = LINE()
 #line = LINE("เมล","พาส")
-#line = LINE('EulkVm03YPzIy1y9cjb3.TAVOkm2wqPizxdXz1JiGmW.8S00JCP4DRsRTBGHcgh3JJRnGxUeoUlXI2iRelhqwrY=')
+#line = LINE('')
 line.log("Auth Token : " + str(line.authToken))
 line.log("Timeline Token : " + str(line.tl.channelAccessToken))
 
@@ -35,8 +35,8 @@ Exc = [line]
 lineMID = line.getProfile().mid
 bot1 = line.getProfile().mid
 RfuBot=[lineMID]
-Family=["ufad8bc98e4811b51115039219b8f8faf",lineMID]
-admin=['ufad8bc98e4811b51115039219b8f8faf',lineMID]
+Family=["u188a6636bea91aba0303d1866408f083",lineMID]
+admin=['u15d6baf1b56b371f98fe441f0abf3ec5',lineMID]
 RfuFamily = RfuBot + Family
 
 protectname = []
